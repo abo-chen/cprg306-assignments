98d19912ff36558e67356c38522d269b43dd161d
Abel Chen <abo.chen@gmail.com>
2023-09-22 10:23:41 -0600

    week 2

---
+++ app/StudentInfo.js
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+  return (
+    <ul>
+      <li>Name: Qingfeng(Abel) Chen</li>
+      <li>Couse: CPRG 306 E</li>
+      <li>Github: <a href="https://github.com/abo-chen/cprg306-assignments">https://github.com/abo-chen/cprg306-assignments</a></li>
+    </ul>
+  );
+}
