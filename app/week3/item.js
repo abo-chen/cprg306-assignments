9d5c99ee3b196953f7812a41354a4ab9d7357a5b
Abel Chen <abo.chen@gmail.com>
2023-10-05 21:14:41 -0600

    assignment 4

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,7 +2,7 @@
 export default function Item({name, quantity, category}){
     return(
         <div className="m-1 p-1 border-x-cyan-600 border-2">
-            <h3>Name: {name}</h3>
+            <h3>{name}</h3>
             <p>Buy {quantity} in {category}</p>
         </div>
     );
